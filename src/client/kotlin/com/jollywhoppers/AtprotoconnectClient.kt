64e82edc38d8b801bec8fd9e1a88ba728536a842
Ewan Croft <git@ewancroft.uk>
2025-12-20 12:06:21 +0000

    init: setup project for Minecraft 1.21.10 with Fabric template

---
+++ src/client/kotlin/com/jollywhoppers/AtprotoconnectClient.kt
@@ -0,0 +1,9 @@
+package com.jollywhoppers
+
+import net.fabricmc.api.ClientModInitializer
+
+object AtprotoconnectClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
